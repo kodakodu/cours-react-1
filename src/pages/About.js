0fd14d8aec039a28d3fb71c47f7fcb71dfc030ae
kodakodu <kodakodu@gmail.com>
2022-08-02 17:31:05 +0200

    Logo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
 const About = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
       <h1>A propos</h1>
       <br />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
 const Home = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
       <h1>Accueil</h1>
     </div>
