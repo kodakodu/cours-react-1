5f765330626ed3ab857cd40252b33bda6b3352ba
kodakodu <kodakodu@gmail.com>
2022-08-03 16:45:54 +0200

    Countries filtre

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Card from './Card';
 
 const Countries = () => {
   const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
 
   useEffect(() => {
     axios
@@ -11,10 +13,19 @@ const Countries = () => {
   }, []);
   return (
     <div className='countries'>
-      <h1>Countries</h1>
+      <ul className='radio-container'>
+        <input
+          type='range'
+          min='1'
+          max='250'
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+      </ul>
       <ul>
-        {data.map((country, index) => (
-          <li key={index}>{country.translations.fra.common}</li>
+        {data.slice(0, rangeValue).map((country, index) => (
+          //   <li key={index}>{country.translations.fra.common}</li>
+          <Card key={index} country={country} />
         ))}
       </ul>
       <button>Button</button>
