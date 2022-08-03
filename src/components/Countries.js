257eea5aec3fce1cb3f44af63587afd2713c790d
kodakodu <kodakodu@gmail.com>
2022-08-03 12:36:44 +0200

    Countries2

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((res) => setData(res.data));
+  }, []);
   return (
-    <div>
+    <div className='countries'>
       <h1>Countries</h1>
+      <ul>
+        {data.map((country, index) => (
+          <li key={index}>{country.translations.fra.common}</li>
+        ))}
+      </ul>
+      <button>Button</button>
     </div>
   );
 };
