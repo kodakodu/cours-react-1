f30ce3c9211763e1d72c7335b00c5a54df6b6000
kodakodu <kodakodu@gmail.com>
2022-08-03 11:51:17 +0200

    Countries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Countries from '../components/Countries';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
@@ -7,7 +8,7 @@ const Home = () => {
     <div>
       <Logo />
       <Navigation />
-      <h1>Accueil</h1>
+      <Countries />
     </div>
   );
 };
